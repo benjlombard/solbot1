5be7392d226df893e3a6441c06c85ca70742df08
Benjamin Lombard <benjamin.lombard@lojelis.com>
2025-08-17 22:54:42 +0200

    add files

--- gitleaks-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gitleaks detect --source . --config .gitleaks.toml --verbose --no-git
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    echo "Gitleaks found secrets!"
-    exit 1
-fi
-exit 0
